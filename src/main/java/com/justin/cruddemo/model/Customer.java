d1fb247ff9ab76e14770c544663453fb24afa18e
Kajal-Kiran-TEL <95415808+Kajal-Kiran-TEL@users.noreply.github.com>
2021-12-02 19:39:27 +0530

    uploaded src

---
+++ src/main/java/com/justin/cruddemo/model/Customer.java
@@ -0,0 +1,34 @@
+package com.justin.cruddemo.model;
+
+import lombok.Data;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Data
+@Entity
+@Table(name = "customer_table")
+public class Customer {
+
+    @Id
+    @Column(name = "ID")
+    private String id;
+
+    @NotNull
+    @Column(name = "customer_name")
+    private String customerName;
+
+    @NotNull
+    @Column(name = "customer_address")
+    private String customerAddress;
+
+    @NotNull
+    @Column(name = "status")
+    private boolean customerStatus;
+
+    @Column(name = "created_at")
+    private String createdAt;
+
+}
